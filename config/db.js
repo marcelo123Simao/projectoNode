87f2bdb296a0ed69db5e57c858a4aec7d5f4532a
Marcelo Simão <simaowork123@gmail.com>
2026-05-27 16:34:09 +0100

    fix mongo + render connection

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-   module.exports = {mongoURI: "mongodb+srv://simao:simao12345@projectonode.2wl20wf.mongodb.net/blogapp?retryWrites=true&w=majority&appName=projectoNODE"} 
+   module.exports = {mongoURI: "mongodb+srv://simao:marcelo12345@@aws.vpqdnco.mongodb.net/?appName=AWS"} 
 } else{
     module.exports = {mongoURI: "mongodb://127.0.0.1:27017/blogapp"}
 }
